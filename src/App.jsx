6525f9e91deea2c10c4c03ee1519261bce9b6d7d
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-09-21 16:39:08 +0100

    Select currency updated, props passed to Currency.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,27 @@ const URL = `http://api.exchangeratesapi.io/latest?access_key=${API_KEY}`
 
 
 function App() {
-
+  const [currencies, setCurrencies] = useState([])
+  const [baseCyrrency, setBaseCurrency] = useState()
+  const [firstOption, setFirstOption] = useState()
   useEffect(() => {
     fetch(URL)
      .then(response => response.json())
-     .then(data => console.log(data))
-    
+     .then(data => {
+       console.log(data)
+       setCurrencies([data.base,...Object.keys(data.rates)])
+       setBaseCurrency(data.base)
+       setFirstOption(Object.keys(data.rates)[0])
+     })
   },[])
  
   return (
     <>
      <h1>Convert currency</h1>
      <div className="container">
-      <Currency defaultValue={1}/>
+      <Currency defaultValue={1} currencies={currencies} selectedCurrency={baseCyrrency}/>
         <FiRepeat/>
-      <Currency defaultValue={0}/>
+      <Currency defaultValue={0} currencies={currencies} selectedCurrency={firstOption}/>
      </div>
     </>
   )
--- src/components/currency.jsx
+++ src/components/currency.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Currency = ({defaultValue}) => {
+const Currency = ({defaultValue,currencies, selectedCurrency}) => {
   return (
     <div>
-        <select name="" id="">
-            <option defaultValue="iva">iva</option>
+        <select name="" id="" value={selectedCurrency} >
+            {currencies.map((currency) => {
+                return (
+                    <option key={currency} defaultValue="currency">{currency}</option>
+                )
+            })}
+            
         </select>
         <input type="number" defaultValue={defaultValue}/>
     </div>
