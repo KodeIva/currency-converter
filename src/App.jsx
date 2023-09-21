7a64e8ac8c414c3431fa8a2df1532c12e829711e
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-09-21 12:16:22 +0100

    Currency component added to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Currency from './components/currency'
 
 function App() {
  
@@ -7,6 +8,11 @@ function App() {
   return (
     <>
      <h1>Convert currency</h1>
+     <div className="container">
+      <Currency/>
+      <button>swap</button>
+      <Currency/>
+     </div>
     </>
   )
 }
---
+++ src/components/currency.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Currency = () => {
+  return (
+    <div>
+        <select name="" id="">
+            <option value="iva">iva</option>
+        </select>
+    </div>
+  )
+}
+
+export default Currency
