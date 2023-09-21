8fdc937fa8170d4b29d61a632ea5a3cc891b8b66
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-09-21 13:13:22 +0100

    URL added and fetched successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Currency from './components/currency'
 import {FiRepeat} from 'react-icons/fi'
+const API_KEY = 'a5dc05191451d8a44274e75ab6675f25'
+const URL = `http://api.exchangeratesapi.io/latest?access_key=${API_KEY}`
+
+
 
 function App() {
- 
 
+  useEffect(() => {
+    fetch(URL)
+     .then(response => response.json())
+     .then(data => console.log(data))
+    
+  },[])
+ 
   return (
     <>
      <h1>Convert currency</h1>
